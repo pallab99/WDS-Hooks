f8529df5b862d57de11769f723acec954bb6b729
Pallab Majumdar <kitkatpallab@gmail.com>
2023-02-24 22:41:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+// function countInitial(){
+//   console.log("Render");
+//   return 0;
+// }
+function App() {
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState('Blue'); //! Use different states to manage different criteria
+  // const [count, setCount] = useState(()=>countInitial()); //!Complex kajer jonno ai method use korte hobe
+
+  function decrementCount() {
+    // setCount(count-1); //!Bad Practice
+    setCount((prevCount) => prevCount - 1); //! Always use previous value to update state
+    // setCount((prevCount) => prevCount - 1);
+  }
+
+  function increamentCount() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  return (
+    <div className="flex justify-center text-9xl">
+      <div className="space-x-8 space-y-8">
+        <button onClick={decrementCount} className="shadow-lg p-8">
+          -
+        </button>
+        <span className="text-blue-500">{count}</span>
+        <span className="text-blue-500">{theme}</span>
+        <button onClick={increamentCount} className="shadow-lg p-8">
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
